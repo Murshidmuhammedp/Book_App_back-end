272184e153717ea2819cd51bd126cbc4254f72e8
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-11-14 09:33:25 +0530

    Url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(cors({
     // origin: "http://localhost:5173",
-    origin: "https://bookverse-app.vercel.app",
+    origin: "https://bookverse-app.vercel.app/",
     credentials: true
 }));
 
